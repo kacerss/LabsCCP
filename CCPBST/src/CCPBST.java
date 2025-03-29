7e4dd9838e22247429b72a4f24612a4a96f8ac00
kacerss <jpenagos888@gmail.com>
2025-03-29 17:17:05 -0400

    Update 5:16

--- CCPBST/src/CCPBST.java
+++ CCPBST/src/CCPBST.java
@@ -6,7 +6,24 @@ public class CCPBST<T extends Comparable<T>>{
         if (this.size == 0){
             this.root.data = element;
         }
+        if (element.compareTo(root.data) > 0){
+            Node curr = traverse(root.right);
+            if (element.compareTo(curr.data) > 0){
+                curr.right.data = element;
+            }
+        }
 
+
+    }
+
+    public Node traverse(Node root){
+        if (root.left != null){
+            traverse(root.left);
+        }
+        if (root.right != null){
+            traverse(root.right);
+        }
+        return root;
     }
     public class Node{
         T data;
