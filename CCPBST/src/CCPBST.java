6d88fbd562bda8dd081ed64f737e845a1879f743
Kacerss <jpenagos888@gmail.com>
2025-04-02 18:05:25 -0400

    add method fixed

--- CCPBST/src/CCPBST.java
+++ CCPBST/src/CCPBST.java
@@ -2,16 +2,21 @@ public class CCPBST<T extends Comparable<T>>{
     Node root;
     int size;
 
-    public void add(T element){
+    public Node add(Node root, T element){
         if (this.size == 0){
-            this.root.data = element;
+            return new Node(element);
         }
-        if (element.compareTo(root.data) > 0){
-            Node curr = traverse(root.right);
-            if (element.compareTo(curr.data) > 0){
-                curr.right.data = element;
-            }
+        if (root.data == element){
+            return root;
         }
+
+        if (element.compareTo(root.data) < 0){
+            root.left = add(root.left, element);
+        } else {
+            root.right = add(root.right, element);
+        }
+
+        return root;
 
     }
 
