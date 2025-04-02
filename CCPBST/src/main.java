306a91b520c349d18b66db0c916c853373717555
Kacerss <jpenagos888@gmail.com>
2025-04-02 18:34:35 -0400

    LAST ADD METHOD

---
+++ CCPBST/src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        CCPBST<Integer> tree = new CCPBST<>();
+        CCPBST<Integer>.Node node = tree.new Node(42); // Correct usage
+
+    }
+}
