817ea91dda4a0a294630a454bca47e6f00feab32
Kacerss <jpenagos888@gmail.com>
2025-04-02 19:46:26 -0400

    Added finished add method

--- CCPBST/src/CCPBST.java
+++ CCPBST/src/CCPBST.java
@@ -2,28 +2,28 @@ public class CCPBST<T extends Comparable<T>> {
     public Node<T> root;
     int size;
 
-    public Node<T> add(Node<T> root, T element) {
-
-        if (root == null) {
-            size++;
-            Node currRoot = new Node(element);
-            return currRoot;
-        }
-
-        // Use compareTo for equality check
-        int cmp = element.compareTo(root.data);
-        if (cmp == 0) {
-            return root;
-        }
-
-        if (cmp < 0) {
-            root.left = add(root.left, element);
-        } else {
-            root.right = add(root.right, element);
-        }
-
-        return root;
+    public Node<T> add(T element) {
+        root = add(root, element);
+        // Optionally, return the reference to the node where element was added.
+        return find(element);
     }
+
+    private Node<T> add(Node<T> node, T element) {
+        if (node == null) {
+            size++;
+            return new Node<>(element);
+        }
+        int cmp = element.compareTo(node.data);
+        if (cmp == 0) {
+            return node; // Duplicate, do not add.
+        } else if (cmp < 0) {
+            node.left = add(node.left, element);
+        } else {
+            node.right = add(node.right, element);
+        }
+        return node;
+    }
+
 
     public class Node<T> {
         T data;
