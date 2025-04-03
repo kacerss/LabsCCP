5a27aa21c4f17e03f2651226e0896ca747ec45ed
Kacerss <jpenagos888@gmail.com>
2025-04-03 11:55:30 -0400

    Added find method, and fixed add method, also the way the generics were working as Node class wasn't using generics

--- CCPBST/src/CCPBST.java
+++ CCPBST/src/CCPBST.java
@@ -1,33 +1,54 @@
 public class CCPBST<T extends Comparable<T>> {
-    public Node root;
-    int size;
 
-    public Node add(Node root, T element) {
+    private Node<T> root;
+    private int size;
+
+    public Node<T> add(T element) {
+        root = add(root, element);
+        // Return the reference to the node where element was added.
+        return find(element);
+    }
+
+    private Node<T> add(Node<T> root, T element) {
         if (root == null) {
             size++;
-            return new Node(element);
+            return new Node<>(element);
         }
-
-        // Use compareTo for equality check
         int cmp = element.compareTo(root.data);
-
         if (cmp == 0) {
-            return root;
-        }
-
-        if (cmp < 0) {
+            return root; // Duplicate, do not add.
+        } else if (cmp < 0) {
             root.left = add(root.left, element);
         } else {
             root.right = add(root.right, element);
         }
-
         return root;
     }
 
-    public class Node {
+    public Node<T> find(T element) {
+        return find(root, element);
+    }
+
+    public Node<T> find(Node<T> node, T element) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = element.compareTo(node.data);
+        if (cmp == 0) {
+            return node;
+        }
+        if (cmp < 0) {
+            return find(node.left, element);
+        } else {
+            return find(node.right, element);
+        }
+
+    }
+
+    private static class Node<T> {
         T data;
-        Node right;
-        Node left;
+        Node<T> left, right;
 
         public Node(T element) {
             this.data = element;
