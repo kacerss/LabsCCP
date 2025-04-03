d7a8626be57b8cb4c99e5e5741d6d74a74e59536
Kacerss <jpenagos888@gmail.com>
2025-04-03 13:04:54 -0400

    Tested Integers and Strings, works correctly, awaiting to use Cards and Decks for it

--- CCPBST/src/Main.java
+++ CCPBST/src/Main.java
@@ -1,21 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        CCPBST<Integer> tree = new CCPBST<>();
-        int[] nums = {10, 15, 5, 40, 0, 50, 70};
-
+        CCPBST<Integer> integer = new CCPBST<>();
+        int[] nums = {50, 10, 15, 5, 40, 0, 60, 70, 90, 1, 3, 123, 65, 4, 7};
         for (int num : nums) {
-            tree.add(num);
+            integer.add(num);
         }
-
-        tree.inOrder();
-
-        tree.find(5);
-
+        integer.inOrder();
         System.out.println();
+        System.out.println("Tree Depth: " + integer.depth());
+        System.out.println("Tree Size: " + integer.size());
 
-        System.out.println(tree.depth());
+        CCPBST<String> string = new CCPBST<>();
+        String[] names = {"Mary", "John", "Jane", "Bob", "May", "Jack", "Juan", "Lucas", "Peter", "Tiana"};
+        for (String name : names) {
+            string.add(name);
+        }
+        string.inOrder();
+        System.out.println();
+        System.out.println("Tree Depth: " + string.depth());
+        System.out.println("Tree Size: " + string.size());
 
-        System.out.println(tree.size());
 
     }
 }
