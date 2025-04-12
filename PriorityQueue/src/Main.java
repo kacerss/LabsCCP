8d89ef95b3e16e897e967efdf768d11d1915b40b
Kacerss <jpenagos888@gmail.com>
2025-04-12 19:12:11 -0400

    Added commentary

--- PriorityQueue/src/Main.java
+++ PriorityQueue/src/Main.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
 
 public class Main {
@@ -11,10 +9,10 @@ public class Main {
             conditions.put("Broken Bone", 3);
             conditions.put("Flu", 2);
             conditions.put("Small Cut", 1);
-
+        // Create a maxHeap and a scanner, initialize with size 20 default.
             MaxHeap<Patient> patientMaxHeap = new MaxHeap<>(20);
             Scanner kb = new Scanner(System.in);
-            var menu = true;
+            var menu = true; // variable to keep track of the loop.
 
             while (menu) {
                 System.out.println();
@@ -24,31 +22,32 @@ public class Main {
                         1. Add a patient.
                         2. Dequeue next patient.
                         3. Exit... """);
+                // take user's input for the menu
                 int input = Integer.parseInt(kb.nextLine());
 
                 switch (input) {
                     case 1:
                         System.out.println("What's the patient's name?");
-                        var name = kb.nextLine();
+                        var name = kb.nextLine(); // get patient's name
                         System.out.println("What's the patient's condition?");
-                        var condition = kb.nextLine();
-                        var priority = conditions.get(condition);
+                        var condition = kb.nextLine(); // get patient's condition
+                        var priority = conditions.get(condition); // get condition's priority
                         // default priority if no priority is found
                         if (priority == 0) {
                             priority = 1;
                         }
-                        Patient patient = new Patient(name, condition, priority);
-                        patientMaxHeap.insert(patient);
+                        Patient patient = new Patient(name, condition, priority); // create patient w the information collected.
+                        patientMaxHeap.insert(patient); // insert patient into the maxHeap
                         System.out.println("Patient inserted: " + patient);
                         break;
                     case 2:
-                        Patient next = patientMaxHeap.extractMax();
-                        String message = (next == null) ? "No patients in queue" : "Next patient: " + next;
+                        Patient next = patientMaxHeap.extractMax(); // extract the next patient and hold it into a variable
+                        String message = (next == null) ? "No patients in queue" : "Next patient: " + next; // check that patient is not null and print the necessary message.
                         System.out.println(message);
                         break;
                     case 3:
                         System.out.println("Goodbye! ...");
-                        menu = false;
+                        menu = false; // exit program
                         break;
                     default:
                         System.out.println("Invalid choice. Please try again.");
